62eb9a50fb490eb09e140c76a0cf84eb19de9afa
ipc103 <iancandysemail@gmail.com>
2016-03-31 18:26:41 -0400

    fixed question validations, added http post method

--- js/app/controllers/FormController.js
+++ js/app/controllers/FormController.js
@@ -1,16 +1,24 @@
-function FormController(this) {
-    this.questions = [];
+function FormController($scope, $http) {
+    this.surveys = []; //only on page load
+    this.survey = {};
+    this.survey.questions = [{}];
 
     this.add = function() {
-        this.questions.push({
-            text: ""
+
+        this.survey.questions.push({
+          title: ""
         });
     };
      this.submit = function() {
-        if (this.questions) {
-          this.questions.push(this.questions);
-          this.questions = '';
-        }
+          $http.post('http://exampleserver.com', this.survey).then(function(response){
+            // Here's where I would put the code from lines 16-20 if I received a response from http://exampleserver.com
+          });
+            this.surveys.push(this.survey);
+            this.survey = {};
+            this.survey.questions = [{}];
+
+            $scope.form.$setUntouched()
+
       };
 }
 
